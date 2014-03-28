4af35bb94a7fb8cb56912d5d4a9178ce03e4890c
Ryan Wang <wongyouth@gmail.com>
2014-03-29 00:42:28 +0800

    upgrade jekyll to 1.5.1

--- Gemfile
+++ Gemfile
@@ -1,18 +1,17 @@
 source "http://ruby.taobao.org"
 
 group :development do
-  gem 'rake', '~> 0.9'
-  gem 'jekyll', '~> 0.12'
-  gem 'rdiscount', '~> 1.6.8'
-  gem 'pygments.rb', '~> 0.3.4'
+  gem 'rake', '~> 10.2.2'
+  gem 'jekyll', '~> 1.5.1'
+  gem 'rdiscount', '~> 2.1.7'
+  gem 'kramdown'
+  gem 'redcarpet'
   gem 'RedCloth', '~> 4.2.9'
-  gem 'haml', '~> 3.1.7'
-  gem 'compass', '~> 0.12.2'
-  gem 'sass-globbing', '~> 1.0.0'
+  gem 'haml', '~> 4.0.5'
+  gem 'compass', '~> 0.12.4'
+  gem 'sass-globbing', '~> 1.1.0'
   gem 'rubypants', '~> 0.2.0'
-  gem 'rb-fsevent', '~> 0.9'
-  gem 'stringex', '~> 1.4.0'
-  gem 'liquid', '~> 2.3.0'
+  gem 'stringex', '~> 2.4.2'
 end
 
-gem 'sinatra', '~> 1.4.2'
+#gem 'sinatra', '~> 1.4.2'
--- plugins/date.rb
+++ plugins/date.rb
@@ -48,30 +48,6 @@ end
 
 module Jekyll
 
-  class Post
-    include Octopress::Date
-
-    # Convert this post into a Hash for use in Liquid templates.
-    #
-    # Returns <Hash>
-    def to_liquid
-      date_format = self.site.config['date_format']
-      self.data.deep_merge({
-        "title"             => self.data['title'] || self.slug.split('-').select {|w| w.capitalize! || w }.join(' '),
-        "url"               => self.url,
-        "date"              => self.date,
-        # Monkey patch
-        "date_formatted"    => format_date(self.date, date_format),
-        "updated_formatted" => self.data.has_key?('updated') ? format_date(self.data['updated'], date_format) : nil,
-        "id"                => self.id,
-        "categories"        => self.categories,
-        "next"              => self.next,
-        "previous"          => self.previous,
-        "tags"              => self.tags,
-        "content"           => self.content })
-    end
-  end
-
   class Page
     include Octopress::Date
 
